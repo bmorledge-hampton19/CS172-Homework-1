da1ed457001bf86f8bdc01bfede3b1b08721d4ca
bmorledge-hampton19 <bmorledge-hampton19@my.whitworth.edu>
2016-09-13 14:08:47 -0700

    Started Project

---
+++ Homework 1/Homework 1/CS 171 Review.cpp
@@ -0,0 +1,21 @@
+/////////////////////
+// Ben Morledge-Hampton
+// CS 172-1
+// 9-13-2016
+// Homework 1
+/////////////////////
+
+// A function that receives no inputs and generates no outputs, but still performs a variety of tasks.
+void ex02();
+
+int main() {
+
+
+
+}
+
+void ex02() {
+
+
+
+}
